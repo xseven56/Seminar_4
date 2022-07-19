28f8539610b85a96b77d8cfbd9ec1df52b217720
xseven56 <xseven56@gmail.com>
2022-07-19 23:44:40 +0500

    Дз к 3 семинару готово

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,28 @@
+﻿//ЛЕЦИЯ 3
+// ВИД № 1
+// void Method1 ()
+// {
+//     Console.WriteLine ("Автор....");
+// }
+// Method1();
+
+// ВИД2
+void Method2(string msg)
+{
+    Console.WriteLine (msg);
+
+}
+// Method2(msg: "Текст сообщения");
+
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while (i<count)
+    {
+        Console.WriteLine (msg);
+        i++;
+    }
+
+}
+Method21(msg: "Текст", count: 4);
+Method21(Count:4, msg);
