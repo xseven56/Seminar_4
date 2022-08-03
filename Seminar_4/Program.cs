a46affc7b458cd34a38cbcc87bee14e63cb37938
xseven56 <xseven56@gmail.com>
2022-08-03 20:40:44 +0500

    Домашнее заданаие к семинару 4

---
+++ Seminar_4/Program.cs
@@ -0,0 +1,45 @@
+﻿// ДОМАШНЕЕ ЗАДАНИЕ К 4 семинару!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!112
+//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+// Console.Write("Введите число A: ");
+//   int numberA = Convert.ToInt32(Console.ReadLine());
+//   Console.Write("Введите число B: ");
+//   int numberB = Convert.ToInt32(Console.ReadLine());
+
+//   int exponentiation = Exponentiation(numberA, numberB);
+//   int Exponentiation(int numberA, int numberB)
+//   {
+//   int result = 1;
+//   for(int i=1; i <= numberB; i++)
+//   {
+//     result = result * numberA;
+//   }
+//     return result;
+// }
+  
+//    Console.WriteLine("Ответ: " + exponentiation);
+
+
+// ЗАДАЧА27// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int s = 0;
+//     while (a > 0)
+//   {
+//   s = s + a % 10;
+//   a = a /10 ;
+//   }
+//  Console.WriteLine(s);
